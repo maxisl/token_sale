04937add31055c888cf71a2112202779e3eafe2d
maximiliansl <maximilian.slapnik@gmail.com>
2020-10-18 20:12:16 +0200

    1. Smoke Test

---
+++ contracts/DappToken.sol
@@ -0,0 +1,16 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+contract DappToken {
+    
+    
+    uint256 public totalSupply;
+
+    //constructor
+    constructor() public {
+        totalSupply = 1000000;
+    }
+    //set the total number of tokens
+    //read the total number of tokens
+
+
+}
